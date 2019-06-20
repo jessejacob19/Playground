8e1be258b45ec2d0e8f93c903e7b87c41eb7da86
jesse collier <jesse.collier97@gmail.com>
2019-06-20 19:37:47 +1200

    finished calculate

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -6,7 +6,31 @@ namespace Playground
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyObject myObject = new MyObject();
+            myObject.number1 = 10;
+            myObject.number2 = 5;
+
+            Console.WriteLine("Number 1 is " + myObject.number1);
+            Console.WriteLine("Number 2 is " + myObject.number2);
+            Console.WriteLine("The Calculation is " + myObject.calculate());
+            Console.ReadLine();
+        }
+    }
+    class MyObject
+    {
+        //can only access methods or variables if they are public
+        //public string myStringProperty { get; set; }
+
+        public int number1 { get; set; }
+        public int number2 { get; set; }
+
+        public int calculate()
+        {
+            return number1 + number2;
+        }
+        public string MyMethod()
+        {
+            return "some value";
         }
     }
 }
